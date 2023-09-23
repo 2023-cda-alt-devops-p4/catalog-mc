e3e9c54cf3b2c2f4a714abf32dc2e682ade0a326
Melcn <melaniearinosy@outlook.com>
2023-09-23 19:23:37 +0200

    feat : init main.ts

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import {createApp } from 'vue';
+import VueRouter from "vue-router";
+
+import './style.css';
+import App from './App.vue';
+
+
+const app = createApp(App);
+
+const routes = Route[];
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory();
+})
+app.mount(#app);
