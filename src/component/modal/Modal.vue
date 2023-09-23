a08371434c32d914017068e8bdfae6dc1f588015
Melcn <melaniearinosy@outlook.com>
2023-09-23 19:39:06 +0200

    feat modal:  init modalglobalcontainer css

--- src/component/modal/Modal.vue
+++ src/component/modal/Modal.vue
@@ -6,9 +6,21 @@
 </script>
 
 <template>
-    
+    <div class="modal-global-container">
+
+    </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 
+    .modal-global-container {
+        position : fixed;
+        top: 0;
+        left: 0;
+        height: 100%;
+        min-height: 100vh;
+        width: 100%;
+        z-index: 500;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
 </style>
