448e68794dab603d484c052ea2a51df5c5364ba9
Melcn <melaniearinosy@outlook.com>
2023-09-23 19:28:03 +0200

    feat : install router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import {createApp } from 'vue';
 import VueRouter from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
 
 import './style.css';
 import App from './App.vue';
@@ -7,9 +8,15 @@ import App from './App.vue';
 
 const app = createApp(App);
 
-const routes = Route[];
+const routes = RouteRecordRow[] = [
+    {path: '/', component: }
+];
 
 const router = VueRouter.createRouter({
-    history: VueRouter.createWebHistory();
+    history: VueRouter.createWebHistory(), 
+    routes
 })
+
+app.use(router);
+
 app.mount(#app);
