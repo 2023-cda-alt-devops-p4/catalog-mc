0256a54d8a5982cd307b2607a7c5126223518d97
Melcn <melaniearinosy@outlook.com>
2023-09-25 15:32:59 +0200

    feat: init sidebarlinks compenant

---
+++ src/component/sidebar/SidebarLink.vue
@@ -0,0 +1,23 @@
+<script>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default {
+    props : {
+        to: { type: String, required: true }
+    },
+    setup(props) {
+    const route = useRoute()
+    const isActive = computed(() => route.path === props.to)
+    return { isActive, collapsed }
+  }
+}
+</script>
+
+<template>
+    <router-link :to="to" class="link" :class="{ active: isActive }">
+    </router-link>
+</template>
+
+<style>
+</style>
