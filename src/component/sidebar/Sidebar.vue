c6e2933b415fbd53641c0ba86a3292a034420ab4
Melcn <melaniearinosy@outlook.com>
2023-09-25 15:29:52 +0200

    feat: create sidebarlinks compenant

--- src/component/sidebar/Sidebar.vue
+++ src/component/sidebar/Sidebar.vue
@@ -1,9 +1,24 @@
 <script>
+import SidebarLink from './SidebarLink'
+import { collapsed, toggleSidebar, sidebarWidth } from './state'
 
+export default {
+  props: {},
+  components: { SidebarLink },
+  setup() {
+    return { collapsed, toggleSidebar, sidebarWidth }
+  }
+}
 </script>
 
 <template>
-    
+    <nav>
+            <ul>
+                <li :class="{ active: currentRoute === '/' }"><router-link to="/">Accueil</router-link></li>
+                <li :class="{ active: currentRoute === '/' }"><router-link to="/">Diagrammes</router-link></li>
+                <li :class="{ active: currentRoute === '/' }"><router-link to="/">A Propos</router-link></li>
+            </ul>
+        </nav>
 </template>
 
 <style scoped>
