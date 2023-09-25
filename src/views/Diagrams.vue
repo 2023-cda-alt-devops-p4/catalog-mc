b26390cffefa10754ada405c7002cc04a1887d41
Melcn <melaniearinosy@outlook.com>
2023-09-25 17:03:00 +0200

    create template in diagrams.vue

--- src/views/Diagrams.vue
+++ src/views/Diagrams.vue
@@ -3,7 +3,7 @@
 export default {
     setup() {
         return {
-            
+
         }
     }
 }
@@ -11,6 +11,22 @@ export default {
 
 <template>
 
+<div class="row">
+    <div v-for="let diagram of diagrams">
+        <div @click="goToModal(diagram)"> <!--add class card horizontal + borderCard-->
+            <div class="card-image">
+                <img [src]="diagram.img">
+            </div>
+
+            <div class="card-stacked">
+                <div class="card-content">
+                    <p>{{ diagram.name }}</p>
+                    <p><small> {{ diagram.resume }}</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
 
