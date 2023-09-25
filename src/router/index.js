0a29cf53daef6ba3ae58ceff7f3e53a194847004
Melcn <melaniearinosy@outlook.com>
2023-09-25 15:35:57 +0200

    feat: create router file

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+const routes = [
+    {
+        path:'/',
+        name: 'Home',
+        component: Home
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
