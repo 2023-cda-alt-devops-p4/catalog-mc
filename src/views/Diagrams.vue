e5d4b1c8347282e80e4150d0bc7eedb46bd73ad6
Melcn <melaniearinosy@outlook.com>
2023-09-26 09:52:53 +0200

    feat diragam vue: create style css

--- src/views/Diagrams.vue
+++ src/views/Diagrams.vue
@@ -31,5 +31,46 @@ export default {
     </div>
 </template>
 
+<style>
+
+    .custom-card {
+        display: flex;
+        border: 1px solid #ccc;
+        margin: 10px;
+        cursor: pointer;
+        transition: transform 0.2s;
+        border-radius: 5px;
+        overflow: hidden;
+    }
+
+    .custom-card:hover {
+        transform: scale(1.05);
+    }
+
+    .custom-card-image {
+        width: 100px; 
+        height: 100px; 
+        flex-shrink: 0;
+        background-size: cover;
+    }
+
+    .custom-card-content {
+        flex: 1;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .custom-card-title {
+        font-size: 18px;
+        font-weight: bold;
+    }
+
+    .custom-card-description {
+        font-size: 14px;
+        color: #666;
+        margin-top: 10px;
+    }
+</style>
 
 
