bee5897e5de74440853f12fbe7a5c179c20baa4d
Melcn <melaniearinosy@outlook.com>
2023-09-26 10:18:06 +0200

    feat test router

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,13 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
+    routes,
 })
+
+const app = Vue.createApp({})
+
+app.use(router)
+
+app.mount('#app')
 
 export default router
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,9 +8,13 @@
 
 
 export default {
-    name: 'Home',
-    components: {
-
+    computed: {
+        username() {
+            return this.$route.params.username
+        },
+    },
+    methods: {
+        
     }
 }
 </script>
