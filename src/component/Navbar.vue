5b23b7935d2b4367a3ef441539b8840d5000d588
Melcn <melaniearinosy@outlook.com>
2023-09-27 16:31:59 +0200

    feat navbar: fix path pb

--- src/component/Navbar.vue
+++ src/component/Navbar.vue
@@ -1,10 +1,46 @@
 <script lang="ts" setup>
-import { TheDropDownNavbar, TheDropDownMenu, TheDropDownItem, TheDropDownDivideBlock } from "vue3-dropdown-navbar";
-import { ref } from "vue";
-
 
 </script>
 
 <template>
+    <div class="content">
+        <div id="logo">
+                Test
+        </div>
+        <nav>
+
+            <ul class="nav-links">
+                <li><a  href="#">Home</a></li>
+                <li><a href="#">Diagrammes</a></li>
+                <li><a href="#">About</a></li><!-- quand on met : c'est pour mettre du code entre ""-->
+            </ul>
+
+            <div id="burger">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+        </nav>
+    </div>
     
 </template>
+
+<style scoped>
+
+.content {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #f0cdcd;
+    min-height: 8vh;
+    font-family: 'Montserrat', sans-serif;
+}
+
+
+ul.nav-links {
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  line-height: 75px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
+
 import  Home  from "./views/Home.vue";
+import "vue3-dropdown-navbar/preflight.css"; 
 
 import './style.css'
 import App from './App.vue';
