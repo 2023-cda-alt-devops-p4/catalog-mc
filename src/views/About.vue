7709d87d41ef92ee4eb7ed0219aac4774f873f25
Melcn <melaniearinosy@outlook.com>
2023-09-30 16:29:57 +0200

    feat: more resources  in about vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,6 +12,7 @@
 
     <p>Ces méthodes de modélisation jouent un rôle crucial dans le développement de projets informatiques pour plusieurs raisons :</p>
     <ul>
+
         <li>Communication et Compréhension : UML et Merise fournissent un moyen clair et visuel de communiquer les idées, les besoins et les exigences d'un projet à l'ensemble de l'équipe de développement. Ils aident à éviter les malentendus et les interprétations erronées.</li>
         <li>Conception Structurée : Ils permettent de décomposer un système complexe en éléments plus simples, ce qui simplifie la conception et la gestion du projet.</li>
         <li>Documentation : Les diagrammes servent de documentation précieuse pour les développeurs, les testeurs et les futurs mainteneurs du logiciel.</li>
@@ -29,6 +30,25 @@
         <li>Diagramme de Flux de Données : C'est un élément clé de la méthode Merise, décrivant comment les données circulent dans le système.</li>
         <li>Diagramme de Modèle Entité-Relation (MER) : Il modélise la structure des données en représentant les entités et leurs relations dans une base de données.</li>
     </ul>
+
+    <h2>Quelques ressources pour aller plus loin... </h2>
+
+    <h3>Pour UML: </h3>
+    <ul>
+        <li>"UML Distilled: A Brief Guide to the Standard Object Modeling Language" par Martin Fowler : Ce livre est une introduction claire et concise à UML, idéal pour les débutants.</li>
+        <li>"UML 2.0 in a Nutshell" par Dan Pilone et Neil Pitman : Un autre guide pratique qui couvre les aspects essentiels de l'UML 2.0.</li>
+        <li>UML Resource Center (Sparx Systems) : <a href="https://www.sparxsystems.com/resources/uml2_tutorial/index.html">Le site Web de Sparx Systems</a> propose de nombreuses ressources, y compris des tutoriels et des articles pour vous aider à maîtriser UML</li>
+        <li>OMG UML Specification : Pour une compréhension approfondie des spécifications UML, <a href="https://www.omg.org/spec/UML">consultez le site Web de l'Object Management Group</a> (OMG), l'organisation qui gère UML. </li>
+    </ul>
+
+    <h3>Pour Merise</h3>
+<ul>
+    <li>"La Méthode Merise" par Jacques Dubois et Denis Guittard : Ce livre offre une introduction complète à la méthode Merise et à la modélisation des systèmes d'information.</li>
+    <li>"Analyse et Conception des Systèmes d'Information" par Bernard Espinasse : Cet ouvrage explique la méthode Merise en mettant l'accent sur l'analyse et la conception des systèmes d'information.</li>
+    <li><a href="https://merise-en-pratique.fr/">Blog "Merise en Pratique"</a> : Ce blog, tenu par des professionnels de l'informatique, propose des articles et des tutoriels pratiques sur l'utilisation de la méthode Merise.</li>
+    <li><a href="http://www-genie-logiciel.com/merise/">Site de Génie Logiciel </a> : Cette ressource en ligne fournit des informations détaillées sur la méthode Merise et son utilisation dans le génie logiciel</li>
+</ul>
+
 </template>
 
 <style scoped>
