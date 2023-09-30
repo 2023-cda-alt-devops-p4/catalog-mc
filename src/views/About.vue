1b4442d57e9cb4ce293cafe024a8fb020a60cdb5
Melcn <melaniearinosy@outlook.com>
2023-09-30 16:19:40 +0200

    feat: uml and meris axplantion in about vue

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <h2>Qu'est-ce que UML et Méthode Merise ?</h2>
+
+    <p>UML (Unified Modeling Language) : UML est un langage de modélisation visuelle standard utilisé pour représenter graphiquement des systèmes logiciels. Il offre un ensemble de notations et de symboles pour décrire la structure, le comportement, l'interaction et d'autres aspects des logiciels. UML facilite la communication entre les concepteurs, développeurs et parties prenantes d'un projet, en offrant un langage commun pour représenter des idées et des concepts.</p>
+
+    <p>Méthode Merise : La méthode Merise est une approche de conception de systèmes d'information largement utilisée en génie logiciel. Elle repose sur des modèles conceptuels, logiques et physiques pour organiser l'information et structurer les systèmes informatiques. Merise aide à planifier, analyser, concevoir et implémenter des bases de données et des systèmes d'information de manière méthodique.</p>
+</template>
+
+<style scoped>
+</style>
